3f195d93f512e3ffc2955bfc0fd8fc52dda36a7c
hdfredl <dat.leef@gmail.com>
2024-02-07 15:05:10 +0100

    created database

---
+++ PokeApi/Database/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokeApi.Models;
+
+namespace PokeApi.Database;
+
+
+public class AppDbContext : DbContext
+{
+    public DbSet<PokemonDbModel> Pokemon { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+}
