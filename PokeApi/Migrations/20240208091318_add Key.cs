fd46b323ab4041eadb6085439ed9be060c7b2b3b
JosefMontanari <josef.montanari94@gmail.com>
2024-02-08 10:40:49 +0100

    Finished!!!

---
+++ PokeApi/Migrations/20240208091318_add Key.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokeApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class addKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pokemons",
+                columns: table => new
+                {
+                    PokemonId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Height = table.Column<int>(type: "int", nullable: false),
+                    Weight = table.Column<int>(type: "int", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pokemons", x => x.PokemonId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pokemons");
+        }
+    }
+}
--- PokeApi/Models/PokemonFromDatabase.cs
+++ PokeApi/Models/PokemonFromDatabase.cs
@@ -1,8 +1,11 @@
-﻿namespace PokeApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PokeApi.Models
 {
 	public class PokemonFromDatabase
 	{
 		// Vad som ska hämtas från Db
+		[Key]
 		public int PokemonId { get; set; }
 		public string Name { get; set; }
 		public int Height { get; set; }
